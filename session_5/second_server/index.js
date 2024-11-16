5c023452375b0e09082ee44f145f5f0c4c968b9f
EndritReqica1 <130557822+EndritReqica1@users.noreply.github.com>
2024-11-16 21:11:12 +0100

    HALA MA SHUME

--- session_5/second_server/index.js
+++ session_5/second_server/index.js
@@ -4,28 +4,24 @@ const fs = require("fs");
 http.createServer((req, res) =>{
     const url = req.url;
     res.writeHead(200, {"Content-type": "text/html"});
-    if(url === "/about"){
-        fs.readFile("views/about.html", function(err, data){
+    function Data(err, data){
 
            
-            res.write(data);
-            res.end();
-        });
+        res.write(data);
+        res.end();
+    }
+    if(url === "/about"){
+        fs.readFile("views/about.html",data );
     
     }else if(url === "/contact"){
-        fs.readFile("views/contact.html", function(err, data){
-           
-            res.write(data);
-            res.end();
-        });
+        fs.readFile("views/contact.html", data);
     
     }else{
-        fs.readFile("views/index.html", function(err, data){
-            res.write(data);
-            res.end();
-        });
+        fs.readFile("views/index.html", data);
     
     }
     
 
 }).listen(8080);
+
+//
