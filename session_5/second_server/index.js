ecfe146b0333711a8b0274dd3964fafe70afbcb2
EndritReqica1 <130557822+EndritReqica1@users.noreply.github.com>
2024-11-16 21:10:19 +0100

    Optimizun

--- session_5/second_server/index.js
+++ session_5/second_server/index.js
@@ -3,26 +3,24 @@ const fs = require("fs");
 
 http.createServer((req, res) =>{
     const url = req.url;
+    res.writeHead(200, {"Content-type": "text/html"});
     if(url === "/about"){
         fs.readFile("views/about.html", function(err, data){
 
-            res.writeHead(200, {"Content-type": "text/html"});
+           
             res.write(data);
             res.end();
         });
     
     }else if(url === "/contact"){
         fs.readFile("views/contact.html", function(err, data){
-
-            res.writeHead(200, {"Content-type": "text/html"});
+           
             res.write(data);
             res.end();
         });
     
     }else{
         fs.readFile("views/index.html", function(err, data){
-
-            res.writeHead(200, {"Content-type": "text/html"});
             res.write(data);
             res.end();
         });
